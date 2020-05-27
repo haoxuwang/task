e1aed4873b5f4ce8e9b61832936515d1f8686462
wanghao <87025569@qq.com>
2020-05-27 10:43:02 +0800

    finish

--- src/com/wanghao/task/Main.java
+++ src/com/wanghao/task/Main.java
@@ -1,35 +1,23 @@
 package com.wanghao.task;
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
-import org.apache.http.NameValuePair;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.impl.client.HttpClientBuilder;
-import org.apache.http.message.BasicNameValuePair;
-import org.apache.http.util.EntityUtils;
-import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 public class Main {
     public static void main(String[] args) {
-        try {
-    HttpClient client = HttpClientBuilder.create().build();               //构建一个Client
-    HttpPost post = new HttpPost("http://localhost/vmt/login/doLogin");    //构建一个POST请求
-    //构建表单参数
-    List<NameValuePair> formParams = new ArrayList<NameValuePair>();
-    formParams.add(new BasicNameValuePair("userName", "admin"));
-    formParams.add(new BasicNameValuePair("password", "123456"));
-    UrlEncodedFormEntity entity = new UrlEncodedFormEntity(formParams, "UTF-8");//将表单参数转化为“实体”
-    post.setEntity(entity);        //将“实体“设置到POST请求里
-    HttpResponse response = client.execute(post);//提交POST请求
-    HttpEntity result = response.getEntity();//拿到返回的HttpResponse的"实体"
-    String content = EntityUtils.toString(result);;            //用httpcore.jar提供的工具类将"实体"转化为字符串打印到控制台
-    System.out.println(content);
-    if(content.contains("登陆成功")){
-        System.out.println("登陆成功！！！");
+    	Map<String, Person> map=new HashMap<>();
+    	map.put("张三", new Person("70", "49", "73", "70"));
+    	map.put("李四", new Person("1094", "49", "73", "70"));
+    	Service service=new Service(map);
+    	List<String[]> rebei=Util.readFile("C:/Users/Administrator/Desktop/666.txt");
+    	Iterator<String[]> iterator= rebei.iterator();
+    	while (iterator.hasNext()) {
+    		String[] task=(String[]) iterator.next();
+    		service.add(task, "2020-05-18");
+			String taskId=service.getId("admin","123456",task);
+			service.finish("admin","123456", taskId, task);
+			service.check("admin", "123456", taskId);
+		}
     }
-        } catch (Exception e) {
-    e.printStackTrace();
-        }
-    }
+    
 }
